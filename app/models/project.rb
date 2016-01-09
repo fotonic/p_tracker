fd53fd464ea24708d3fe33dee9dc6f482a8374ae
Chip Eyler <freqnlabs@gmail.com>
2016-01-09 18:35:15 -0500

    Delete project's tickets when a project is deleted

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,7 @@
 class Project < ActiveRecord::Base
 	validates :name, presence: true
 	validates :description, length: { minimum: 10}
-	has_many :tickets
+	has_many :tickets, dependent: :delete_all
 
 	def name=(s)
 		# binding.pry
